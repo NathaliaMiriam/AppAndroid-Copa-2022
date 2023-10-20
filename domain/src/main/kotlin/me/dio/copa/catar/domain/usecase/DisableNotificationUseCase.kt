7b66963dbfde265f2a419d68fc972cfb5757edd7
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-10-20 12:40:56 -0300

    Create UseCases

--- domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/DisableNotificationUseCase.kt
+++ domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/DisableNotificationUseCase.kt
@@ -3,6 +3,10 @@ package me.dio.copa.catar.domain.usecase
 import me.dio.copa.catar.domain.repositories.MatchesRepository
 import javax.inject.Inject
 
+// tenho que ir no repository (onde vai ter a interface das partidas)...
+// desabilitar as notificações da listagem
+// cada notificação tem uma identificação -> (id: String)
+
 class DisableNotificationUseCase @Inject constructor(
     private val repository: MatchesRepository
 ) {
--- domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/GetMatchesUseCase.kt
+++ domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/GetMatchesUseCase.kt
@@ -5,9 +5,13 @@ import me.dio.copa.catar.domain.model.Match
 import me.dio.copa.catar.domain.repositories.MatchesRepository
 import javax.inject.Inject
 
+// tenho que ir no repository (onde vai ter a interface das partidas), retornar a lista de matches...
+
 class GetMatchesUseCase @Inject constructor(
     private val repository: MatchesRepository
 ) {
+
+    // é uma função que indica que o elemento vai ser executado de maneira assíncrona, forma de dizer que ela necessita de um tratamento especial por ter essa caracteristica
     suspend operator fun invoke(): Flow<List<Match>> {
         return repository.getMatches()
     }
